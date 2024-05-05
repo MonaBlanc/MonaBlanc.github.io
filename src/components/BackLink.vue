944fa6496a413f5779283b61090d3407dde85dcd
kim <email@example.com>
2024-05-05 19:07:31 +0200

    ASCII arrow

--- src/components/BackLink.vue
+++ src/components/BackLink.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tree">
     <router-link :to="RouteName.Home" class="underline-hover-effect" v-if="$route.name !== 'Home'">
-      < Back to Home
+      &#x21A9; Back to Home
     </router-link>
   </div>
 </template>
